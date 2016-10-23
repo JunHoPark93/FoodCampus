d2103d0cdaff32b3195487cf2183f1f407aa50f5
JunHo <pjh374968@gmail.com>
2016-10-23 20:39:00 +0900

    테스트

--- app/src/main/java/com/jarvis/foodcampus/model/UserModel.java
+++ app/src/main/java/com/jarvis/foodcampus/model/UserModel.java
@@ -8,9 +8,6 @@ package com.jarvis.foodcampus.model;
 public class UserModel {
     private long id;
     private String nickName;
-    //private int test;
-    //
-    //
 
     public UserModel(long id, String nickName) {
         this.id = id;
