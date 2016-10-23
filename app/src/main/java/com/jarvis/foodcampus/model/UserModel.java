243f8d9893139ec5e817d2e471b1ed2d4f38bd6d
JunHo <pjh374968@gmail.com>
2016-10-23 11:16:08 +0900

    테스트

--- app/src/main/java/com/jarvis/foodcampus/model/UserModel.java
+++ app/src/main/java/com/jarvis/foodcampus/model/UserModel.java
@@ -4,10 +4,11 @@ package com.jarvis.foodcampus.model;
  * Created by JunHo on 2016-10-22.
  */
 
-// UserModel 정의
+// UserModel 정의 .
 public class UserModel {
     private long id;
     private String nickName;
+    //private int test;
 
     public UserModel(long id, String nickName) {
         this.id = id;
