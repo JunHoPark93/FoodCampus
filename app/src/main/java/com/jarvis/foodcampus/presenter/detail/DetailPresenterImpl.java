f3532737a14ff3296148fae8fabadb9745287bf4
JunHo <pjh374968@gmail.com>
2016-11-16 22:25:56 +0900

    DetailPresenter에서 order 기능 추가

--- app/src/main/java/com/jarvis/foodcampus/presenter/detail/DetailPresenterImpl.java
+++ app/src/main/java/com/jarvis/foodcampus/presenter/detail/DetailPresenterImpl.java
@@ -1,9 +1,12 @@
 package com.jarvis.foodcampus.presenter.detail;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.jarvis.foodcampus.DB.DatabaseHelper;
@@ -12,8 +15,18 @@ import com.jarvis.foodcampus.model.DetailModel;
 import com.jarvis.foodcampus.model.FoodModel;
 import com.jarvis.foodcampus.view.detail.DetailView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by JunHo on 2016-11-05.
@@ -29,6 +42,7 @@ public class DetailPresenterImpl implements DetailPresenter {
     private DatabaseHelper databaseHelper;
     private FoodModel[] foodModels;
     private Drawable resIcon;
+    private SharedPreferences sharedPreferences;
 
     private ArrayList<String> arrayGroup = new ArrayList<>();
     private HashMap<String, ArrayList<DetailModel>> arrayChild = new HashMap<>();
@@ -83,7 +97,10 @@ public class DetailPresenterImpl implements DetailPresenter {
         database.close();
     }
 
+    // 스트링 변수 위로 뺴고 order 테이블도 같이 쿼리문짜줘
     public void getSingleRestaurantData() {
+
+
         String sql = "SELECT * FROM " + "restaurant " + "WHERE "+"restaurant_id = "+ "'" + restaurant + "'";
         Cursor result = database.rawQuery(sql, null);
 
@@ -102,6 +119,21 @@ public class DetailPresenterImpl implements DetailPresenter {
         result.close();
         database.close();
 
+    }
+
+    public void getOrderData() {
+        int ordernum = 0;
+        String sql = "SELECT COUNT(*) FROM " + "ordernum " + "WHERE restaurant_id = " + "'" + restaurant + "'";
+        Cursor result = database.rawQuery(sql, null);
+
+        if(result.moveToFirst()) {
+            ordernum = result.getInt(0);
+        }
+
+        detailView.setOrder(ordernum);
+
+        result.close();
+        database.close();
     }
 
     private Drawable getRestaruantIcon(String category) {
@@ -182,42 +214,85 @@ public class DetailPresenterImpl implements DetailPresenter {
 
             }
             detailView.add(arrayGroup, arrayChild);
-            getSingleRestaurantData();
+            getSingleRestaurantData(); // 디테일로 정보 뿌리기
+
+            database = databaseHelper.getReadableDatabase(); // getSingleRestaurantData()안에서 close 해서 다시 open
+            getOrderData();
 
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("에러터지는건 등록된 음식이 하나도없어서 그럼");
             Toast.makeText(context, "음식 정보가 하나도 없습니다", Toast.LENGTH_LONG).show();
         }
-
-        /*
-        디폴트 코드
-        arrayGroup.add("Pizza");
-        arrayGroup.add("Chicken");
-        arrayGroup.add("Pork");
-
-        ArrayList<DetailModel> arrayPizza = new ArrayList<DetailModel>();
-        ArrayList<DetailModel> arrayChicken = new ArrayList<DetailModel>();
-        ArrayList<DetailModel> arrayPork = new ArrayList<DetailModel>();
-
-        arrayPizza.add(new DetailModel(arrayGroup.get(0),"콤비네이션","L",13000));
-        arrayPizza.add(new DetailModel(arrayGroup.get(0),"불고기","M",14000));
-        arrayPizza.add(new DetailModel(arrayGroup.get(0),"고구마","XL",15000));
-        arrayChicken.add(new DetailModel(arrayGroup.get(1),"순살","1마리",15000));
-        arrayChicken.add(new DetailModel(arrayGroup.get(1),"후라이드","1마리",16000));
-        arrayChicken.add(new DetailModel(arrayGroup.get(1),"양념","1마리",17000));
-        arrayPork.add(new DetailModel(arrayGroup.get(2),"일반","XL",15000));
-        arrayPork.add(new DetailModel(arrayGroup.get(2),"양념","XL",15000));
-        arrayPork.add(new DetailModel(arrayGroup.get(2),"직화","XL",15000));
-
-
-        arrayChild.put(arrayGroup.get(0),arrayPizza);
-        arrayChild.put(arrayGroup.get(1),arrayChicken);
-        arrayChild.put(arrayGroup.get(2),arrayPork);
-
-        detailView.add(arrayGroup, arrayChild);
-        */
-
     }
 
+    @Override
+    public void sendOrderNum() {
+
+        sharedPreferences = context.getSharedPreferences("login", MODE_PRIVATE);
+        final String api_id = sharedPreferences.getString("serial_number",null);
+
+        class SendOrderNumber extends AsyncTask<String, Void, String> {
+
+            private URL url;
+
+            public SendOrderNumber(String url) throws MalformedURLException {
+                this.url=new URL(url);
+            }
+
+            private String readStream(InputStream in) throws IOException {
+                StringBuilder jsonHtml = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+                String line = null;
+
+                while((line = reader.readLine()) != null)
+                    jsonHtml.append(line);
+
+                reader.close();
+                return jsonHtml.toString();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+
+                    String postData = "api_id="+api_id+"&restaurant_id="+restaurant;
+
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                    conn.setRequestMethod("POST");
+                    conn.setConnectTimeout(10000);
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+                    OutputStream outputStream = conn.getOutputStream();
+                    outputStream.write(postData.getBytes("UTF-8"));
+                    outputStream.flush();
+                    outputStream.close();
+                    String result = readStream(conn.getInputStream());
+                    conn.disconnect();
+
+                    return result;
+                }
+                catch (Exception e) {
+                    Log.i("PHPRequest", "request was failed.");
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                System.out.println("주문전송끝");
+                super.onPostExecute(s);
+            }
+        }
+
+        SendOrderNumber sendOrderNumber = null;
+
+        try {
+            sendOrderNumber = new SendOrderNumber("http://117.17.158.237:3389/index.php/mOrder");
+            sendOrderNumber.execute();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
 }
